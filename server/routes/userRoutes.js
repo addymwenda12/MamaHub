716c532f60b0780feb88cbcc172fb3885a236490
addymwenda12 <muneneaddy@gmail.com>
2024-07-03 13:10:22 +0300

    Removed friends model

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -9,10 +9,7 @@ import { verifyToken } from "../middleware/auth.js";
 const router = express.Router();
 
 /* READ */
-router.get("/:id", verifyToken, getUser);
-router.get("/:id/friends", verifyToken, getUserFriends);
-
-/* UPDATE */
-router.patch("/:id/:friendId", verifyToken, addRemoveFriend);
+router.get('/profile', verifyToken, getUserProfile);
+router.put('/profile', verifyToken, updateUserProfile);
 
 export default router;
