a7cdbe20a944a0d995307fab63f7907784f5a9db
addymwenda12 <muneneaddy@gmail.com>
2024-07-03 13:17:12 +0300

    Added profile route

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -9,7 +9,14 @@ import { verifyToken } from "../middleware/auth.js";
 const router = express.Router();
 
 /* READ */
+router.get("/:id", verifyToken, getUser);
+router.get("/:id/friends", verifyToken, getUserFriends);
 router.get('/profile', verifyToken, getUserProfile);
+
+/* UPDATE */
+router.patch("/:id/:friendId", verifyToken, addRemoveFriend);
+
+/* PUT */
 router.put('/profile', verifyToken, updateUserProfile);
 
 export default router;
