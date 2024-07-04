c2b06ad67af27012f680fcbfa2e6c459195d6e67
RaphaelKaira <113975698+Raphael-pix@users.noreply.github.com>
2024-07-04 18:39:31 +0300

    created post container component
    This component displays the posts that the user has shared

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,10 +1,18 @@
 import Banner from "../../components/banner/Banner"
-
+import PostItem from "../../components/posts/Post-item"
+import './home.css'
 
 const Home = () => {
   return (
     <section className='Home-page'>
       <Banner/>
+      <section className="sharedPost">
+        <h1 className="title">shared journeys</h1>
+        <PostItem/>
+        <PostItem/>
+        <PostItem/>
+        <PostItem/>
+      </section>
     </section>
   )
 }
