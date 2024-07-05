aad03c160a92ead36f71e8c3d37a4efcc831e6ba
addymwenda12 <muneneaddy@gmail.com>
2024-07-05 12:49:09 +0300

    Added controller function for comments

---
+++ server/controllers/comments.js
@@ -0,0 +1,31 @@
+import Comment from '../models/Comment'
+import Post from '../models/Post'
+
+export const addComment = async (req, res) => {
+  try {
+    const { postId, text } = req.body;
+    const userId = req.user.id;
+
+    const post = await Post.findById(postId);
+    if (!post) {
+      return res.status(404).json({ error: "Post not found" });
+    }
+
+    const newComment = new Comment({ userId, postId, text });
+    const savedComment = await newComment.save();
+
+    res.status(201).json(savedComment);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+export const getComments = async (req, res) => {
+  try {
+    const { postId } = req.params;
+    const comments = await Comment.find({ postId }).populate('userId', 'firstName lastName');
+    res.status(200).json(comments);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
